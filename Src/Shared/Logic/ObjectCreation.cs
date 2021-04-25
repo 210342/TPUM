1b816396379d9992f887a7e3eb96f1f429a726da
krzysztofadam <krzysztofadamkrzeszewski@gmail.com>
2021-04-25 09:39:53 +0200

    feat(logic): moved author and book creation functions into separate logic project

---
+++ Src/Shared/Logic/ObjectCreation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TPUM.Shared.Data.Core;
+using TPUM.Shared.Data.Entities;
+
+namespace TPUM.Shared.Logic
+{
+    public static class Logic
+    {
+        public static void AddAuthor(IRepository repository)
+        {
+            Random _rng = new Random();
+            Task.Run(() =>
+            {
+                int id;
+                do
+                {
+                    id = _rng.Next(5000);
+                }
+                while (repository.GetAuthorById(id) != null);
+
+                IAuthor newAuthor = Data.DataFactory.CreateObject<IAuthor>();
+                newAuthor.Id = id;
+                newAuthor.FirstName = $"{id} - {nameof(IAuthor.FirstName)}";
+                newAuthor.LastName = $"{id} - {nameof(IAuthor.LastName)}";
+                newAuthor.NickName = $"{id} - {nameof(IAuthor.NickName)}";
+                repository.AddAuthor(newAuthor);
+            });
+        }
+
+        public static void AddBook(IRepository repository)
+        {
+            Random _rng = new Random();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                    int id;
+                    do
+                    {
+                        id = _rng.Next(5000);
+                    }
+                    while (repository.GetBookById(id) != null);
+                    IBook newBook = Data.DataFactory.CreateObject<IBook>();
+                    newBook.Id = id;
+                    newBook.Title = $"{id} - {nameof(IBook.Title)}";
+                    repository.AddBook(newBook);
+                }
+            });
+        }
+    }
+
+}
